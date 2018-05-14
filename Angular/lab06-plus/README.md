c570c1e00332565ba6b308bebb174128e64c9763
Mario Estrada <marioestradarosa@yahoo.com>
2018-05-13 22:46:22 -0600

    Lab06-plus was added, contains bonus

---
+++ Angular/lab06-plus/README.md
@@ -0,0 +1,14 @@
+# Solution for Lab06
+May 13 2018
+
+You were required go implement the following:
+
+- Guard the routes based upon authentication status [account/pin] by implementing canActivate
+  in its own service
+- Reset by zero the amount for a Deposit/Withdrawl 
+- Make child routes for the main ATM Operation
+- Create a Router Module that holds the router configuration
+
+In order to execute it, you should have the ATM Web Service from  **AtmWebServiceMongo** from the TypeScript folder already running on port 3000. then either clone bootcamp repo or git pull to update if you already cloned it, then run `ng serve --open` to run the application and open your browser.
+
+NOTE: Keep note that this Angular Cient Application still needs the unsecured ATM WEB SERVICE MONGO edition from the TypeScript project. 
---
+++ Angular/lab06-plus/src/app/app.module.ts
@@ -0,0 +1,44 @@
+/*
+  Instructor : Mario Estrada Rosa
+  Email  : marioestrada@eycgrupo.com
+  Date   : April 2018
+  Class  : Introduction to Angular Framework
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RoutingModule } from './router.module';
+
+import { AtmServiceService } from './services/atm-service.service';
+import { GuardServiceService } from './services/guard-service.service';
+
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
+import { AtmoperationsComponent } from './components/atmoperations/atmoperations.component';
+import { AuthenticateComponent } from './components/authenticate/authenticate.component';
+import { MainopComponent } from './components/mainop/mainop.component';
+
+ 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TransactionListComponent,
+    AtmoperationsComponent,
+    AuthenticateComponent,
+    MainopComponent
+  ],
+  
+  imports: [
+    BrowserModule  ,
+    HttpClientModule,
+    RoutingModule,
+    ReactiveFormsModule
+  ],
+  //We have to import the service as a provider in the NGModule ecosystem
+  //otherwise it won´t be available in the Angular Application
+  providers: [AtmServiceService,GuardServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
